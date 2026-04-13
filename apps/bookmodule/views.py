21249c986792b0f1f79f1baed068b4101f5fc0cf
DeemAlnughmush <deemalnughmush@gmail.com>
2026-04-13 06:26:04 +0300

    Lab 7 - Django ORM queries

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Book
 
 def index(request): 
     return render(request, "bookmodule/index.html") 
@@ -50,3 +51,25 @@ def __getBooksList():
     book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'} 
     book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'} 
     return [book1, book2, book3]
+
+def add_book(request):
+    # mybook = 
+    Book.objects.create(
+        title='Continuous Delivery',
+        author='J.Humble and D. Farley',
+        edition=1
+    )
+    # mybook.save() 
+
+    return HttpResponse("Book Added Successfully")
+
+def simple_query(request):
+    mybooks = Book.objects.filter(title__icontains='the')
+    return render(request, 'bookmodule/bookList.html', {'books': mybooks})
+
+def complex_query(request): 
+    mybooks=books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10] 
+    if len(mybooks)>=1: 
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks}) 
+    else: 
+        return render(request, 'bookmodule/index.html')
